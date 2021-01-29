98f6d7bec48a5c94ccab0d2145c56f21e7cd0f74
hhq <1710308677@q.com>
2021-01-29 15:06:51 +0800

    init

---
+++ Source/HqLib.h
@@ -0,0 +1,18 @@
+//
+//  HqLib.h
+//  HqPrivatePodLib
+//
+//  Created by hehuiqi on 2021/1/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HqLib : NSObject
+
+- (NSString *)version;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/HqLib.m
@@ -0,0 +1,16 @@
+//
+//  HqLib.m
+//  HqPrivatePodLib
+//
+//  Created by hehuiqi on 2021/1/29.
+//
+
+#import "HqLib.h"
+
+@implementation HqLib
+
+- (NSString *)version{
+    return @"1.0";
+}
+
+@end
